271cf161ab62df557d1c6aff519efa55e776909d
feehe21 <33964166+feehe21@users.noreply.github.com>
2023-04-07 00:06:34 -0400

    Update MS5803.h
    fixed to match .c file

--- lib/MS5803.h
+++ lib/MS5803.h
@@ -4,6 +4,6 @@ uint16_t readProm(uint8_t address);
 
 uint32_t readAdc(uint8_t address);
 
-uint32_t readD1(uint8_t address);
+uint32_t readD1();
 
-uint32_t readD2(uint8_t address);
+uint32_t readD2();
