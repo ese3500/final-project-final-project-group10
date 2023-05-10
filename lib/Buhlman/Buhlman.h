b38e3b10795da1c080ffe4fdd39c562d593092e3
feehe21 <33964166+feehe21@users.noreply.github.com>
2023-05-09 21:32:52 -0400

    Update Buhlman.h

--- lib/Buhlman/Buhlman.h
+++ lib/Buhlman/Buhlman.h
@@ -10,14 +10,11 @@
 
 void initializeCalculations();
 int calculateDepthFromPressure(int pressure);
-int calculateHydrostaticPressureFromDepth(int depth);
-void setPPOfCompartment(int compartmentIdx, float pressure);
 float calculatePPLung(float currentPressure);
 float calculateCompartmentPPOtherGasses(int timeSec, float compthalfTimeSec, float currentComptPPOtherGasses, float currentLungPPOtherGasses);
 float ascendToPPForCompt(int compartmentIdx, float comptPP);
 int getNoDecoStopMinutes(float currentPressure);
-int getNoDecoStopMinutesIdx(int comptIdx, float currentPressure);
-bool getDecoStopNeeded(float ascendToPP);
+int getNoDecoStopMinSingleCompartment(int comptIdx, float currentPressure);
 int minutesToTargetPressure(int targetPressure);
 float calculateAscentRate(int timeAtCurrentDepth, int previousDepth, int currentDepth);
 void advanceCalculations(int oldDepth, int newPressure, int totalDiveTime);
